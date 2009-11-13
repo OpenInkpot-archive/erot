efb7bec5bd5c1d4cca9f6ddae82fc7a0e83b2842
Mikhail Gusarov <dottedmag@dottedmag.net>
2009-11-13 13:26:08 +0600

    Update copyrights

--- src/erot.c
+++ src/erot.c
@@ -1,3 +1,24 @@
+/*
+ * service.c - Ecore_Con-compatible service handling.
+ *
+ * Copyright © 2009 Alexander Kerner <lunohod@openinkpot.org>
+ * Copyright © 2009 Mikhail Gusarov <dottedmag@dottedmag.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <stdlib.h>
 #include <err.h>
 #include <stdio.h>
